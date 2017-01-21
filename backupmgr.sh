dc1d65fdae483a7015e87b410d2a93db3683542d
Manuel Alcocer Jiménez <manuel@alcocer.net>
2017-01-21 11:21:36 +0100

    desarrollo incial y pruebas

---
+++ backupmgr.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Variables globales
+REMOTE_HOSTS=()
+BASE_STOR=${BASE_STOR:-/BackUps}
+REMOTE_HOSTS_FILE=${REMOTE_HOSTS_FILE:-/etc/backupMgr/hosts.list}
+
+# Captura de prámetros
+COMMAND=$1
+
+function ERROREXIT(){
+    printf 'Hubieron errores...Saliendo\n'
+    exit 1
+}
+
+#
+function getHostIPS(){
+    :
+}
+
+function checkConfig(){
+    if [[ ! -f ${REMOTE_HOSTS_FILE} ]]; then
+        printf "No existe el fichero de hosts remotos ${REMOTE_HOSTS_FILE}\n"
+        ERROREXIT
+    fi
+    if [[ ! -d ${BASE_STOR} ]]; then
+        printf "No existe la ruta de almacenamiento local ${BASE_STOR}\n"
+        ERROREXIT
+    fi
+
+}
+
+function main(){
+    checkConfig
+    if [[ ${COMMAND,,} == 'completa' ]]; then
+        :
+    fi
+}
